974307264532fe3973b03951e454cbba42288a39
Will Ginsberg <will.j.ginsberg@gmail.com>
2023-11-22 12:26:37 -0500

    add override for manifest v2

--- packages/xtensio/src/plugins/ManifestGenPlugin.ts
+++ packages/xtensio/src/plugins/ManifestGenPlugin.ts
@@ -34,13 +34,37 @@ class ManifestGenPlugin {
             )
             if (!manifestExport)
               console.error(`[${PLUGIN_NAME}]: Default export not found!`)
+
+            const isMV2 = manifestExport.manifest_version === 2
+
+            const manifestVersion2Overrides = isMV2
+              ? {
+                  background: {
+                    scripts: ["background.js"],
+                    persistent: false,
+                    type: "module"
+                  },
+                  action: undefined,
+                  browser_action: {
+                    default_popup: "popup.html",
+                    options_ui: {
+                      page: "settings.html",
+                      open_in_tab: true
+                    }
+                  },
+                  browser_specific_settings:
+                    this.options.extend.browser_specific_settings
+                }
+              : {}
+
             const manifestObj = {
               ...(manifestExport || {}),
               ...this.options.extend,
               permissions: [
                 ...(manifestExport.permissions || []),
                 ...(this.options.extend.permissions || [])
-              ]
+              ],
+              ...manifestVersion2Overrides
             }
             const outSource = JSON.stringify(manifestObj, null, 2)
             compilation.deleteAsset(this.options.filename)
